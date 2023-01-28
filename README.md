d0f3a0591a8f283432a6cb3a38f3ccedf8d0bb3d
Eric Miller <hi@simple.gy>
2023-01-28 10:53:54 -0800

    working shell Obsidian plugin

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-An Obsidian compatible version of the [Emoji Magic](https://github.com/SimplGy/emoji-magic) Chrome Extension.
+> Note: This is an [Obsidian](https://obsidian.md/)-compatible fork of the [Emoji Magic](https://github.com/SimplGy/emoji-magic) Chrome Extension I wrote.
+
+
+
+
+
+## Developing this Plugin
+
+
+### Building
+
+```
+npm install
+npm run dev
+```
+
+(for auto refreshing) install `git clone https://github.com/pjeby/hot-reload.git` and turn it on
+
+### Releasing
+
+1. Update version in `manifest.json` and `package.json`
+2. commit, push.
+3. To put on GitHub, do the following tag stuff:
+
+(replace with the new version number)
+
+```
+git tag -a 0.1.0 -m "0.1.0"
+git push origin 0.1.0
+```
+
+> This will trigger `.github/workflows/release.yml`.
+> 
+> Verify the workflow is running [here](https://github.com/SimplGy/obsidian-open-file-by-magic-date/actions).
+> Verify [releases here](https://github.com/SimplGy/obsidian-open-file-by-magic-date/releases)
+
+4. (you're done) simply doing a github release and running release.yml will make the new version of the plugin available on the Obsidian marketplace. Nice!
 
 
 
@@ -13,9 +49,10 @@ An Obsidian compatible version of the [Emoji Magic](https://github.com/SimplGy/e
 
 
 
+## Contributing
 
+Contributions welcome. See CONTRIBUTING.md.
 
-
-### Disclaimer
+## Disclaimer
 
 This is not an officially supported Google product.
---
+++ src/main.ts
@@ -0,0 +1,104 @@
+/*
+Copyright 2019 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { App, MarkdownView, Plugin, PluginSettingTab, Setting, TFile, moment } from 'obsidian';
+
+interface MyPluginSettings {
+	foo: string;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	foo: 'Sounds good.'
+};
+
+// ---------------------------------------------------- Plugin Definition
+export default class MyPlugin extends Plugin {
+	
+	settings: MyPluginSettings;
+	
+	async onload() {
+		await this.loadSettings();
+		console.log('loading ' + this.manifest.name);
+		this.addSettingTab(new SettingsTab(this.app, this));
+		this.resetCommands();
+	}
+
+	onunload() {
+		console.log('unloading ' + this.manifest.name);
+	}
+
+	async loadSettings() {
+		this.settings = {...DEFAULT_SETTINGS, ...await this.loadData()};
+	}
+
+	// note: this is called ~every keystroke, so be aware
+	async saveSettings() {
+		await this.saveData(this.settings);
+		// update the commands, which is what hotkeys are set against
+		this.resetCommands();
+	}
+
+
+	// ---------------------------------------------------- Commands
+	resetCommands() {
+		// if you "add" with the same id, it works as an "update"
+		this.addCommand({
+			id: 'emoji-magic-copy', // should not change over time
+			name: `Find an emoji and copy it to clipboard`,
+			callback: () => {
+				console.log('Hello from a plugin command');
+			}
+		});
+		this.addCommand({
+			id: 'emoji-magic-insert', // should not change over time
+			name: `Find an emoji and insert it where your cursor is`,
+			callback: () => {
+				console.log('Hello from a plugin command');
+			}
+		});
+	}
+}
+
+
+
+// ---------------------------------------------------- Settings Screen
+class SettingsTab extends PluginSettingTab {
+	
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display() {
+		const { containerEl } = this;
+		containerEl.empty();
+		containerEl.createEl("h2", { text: this.plugin.manifest.name });
+		
+		const setting = new Setting(this.containerEl);
+	  setting.setName("Setting for 'foo'");
+		setting.addText(cb => {
+			cb
+				.setPlaceholder("placeholder text")
+				.setValue(this.plugin.settings.foo)
+				.onChange(value => {
+					this.plugin.settings.foo = value;
+					this.plugin.saveSettings();
+				});
+		});		
+	}
+}
