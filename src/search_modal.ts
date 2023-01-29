45af11b5a050ea6fcee622f2ee42e46f6ac7daf9
Eric Miller <hi@simple.gy>
2023-01-28 23:32:00 -0800

    rm import

--- src/search_modal.ts
+++ src/search_modal.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { App, Modal, Plugin } from 'obsidian';
+import { Modal, Plugin } from 'obsidian';
 
 import { MyPluginSettings, Emoji } from './interfaces';
 
